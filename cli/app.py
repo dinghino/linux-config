90b48613adc6f0993ad6634a634a6128654973b8
Daniele Ding <dinghino@gmail.com>
2017-05-03 19:11:51 +0200

    app execute scripts + save on json
    app can now execute scripts and save persistent data on existing state json file
    
    store paths to commonly used locations/files
    duplicate loaded state.json to allow easy and dump-proof edit on runtime
    add utility methods to work with scripts
    add utility methods to work with doubled app state structure
    refactor update_option to save installation date when called

--- cli/app.py
+++ cli/app.py
@@ -1,12 +1,17 @@
 """
 Application manager.
 """
+import copy
 import json
 import os
+import shlex
+import subprocess
 import time
 from datetime import datetime
 
 import click
+
+APP_FOLDER_NAME = 'cli'
 
 
 class AppManager(object):
@@ -15,6 +20,16 @@ class AppManager(object):
     """
 
     def __init__(self):
+        # absolute path of the python application
+        self._app_dir = os.path.dirname(__file__)
+        # absolute path to the root folder of the application. Uses os.path
+        # instead of pathlib to try and mantain python 2.7 compatibility.
+        # this file is in <root>/cli/ so we need to walk up one
+        self._root_dir = os.path.dirname(self._app_dir)
+        # folder where the scripts are
+        self.scripts_dir = os.path.join(self._root_dir, 'scripts')
+        # path to the app state file (state.json)
+        self._app_state_path = os.path.join(self._app_dir, 'state.json')
         # verbosity level, for logging and stuff
         self.verbose = 0
         # root of the project, so the program can access files
@@ -24,19 +39,50 @@ class AppManager(object):
         # wether the setup has been done or not
         self.setup_done = False
         # state of the application functionalities, used to generate menu
-        # and do callbacks
-        with open(os.path.join(os.path.dirname(__file__), 'state.json')) as fo:
-            self.options = json.load(fo)
+        # and do callbacks.
+        # self._options is exactly the data stored in the json, that have to
+        # be kept as it is and updated only with serializable key-values,
+        # while the public `options` can be worked on at runtime
+        # (setting callback functions, adding other options etc...)
+        with click.open_file(self._app_state_path) as fo:
+            self._options = json.load(fo)
+            self.options = copy.deepcopy(self._options)
+
         # setup the callbacks
         for opt in self.options:
             opt['callback'] = self._generate_callback(opt)
+
+    def get_script_path(self, filename):
+        """
+        Return the absolute path to the script file inside the scripts folder.
+        """
+        return os.path.join(self.scripts_dir, filename)
+
+    def execute(self, script, cb=lambda: None, *args, **kwargs):
+        """
+        Execute the given script, then call the optional callback passing
+        the script output as first argument and all extra arguments given.
+        """
+        # NOTE: For now assume the the <script> argument is a path to valid
+        # script and try to call it. Later we'll check if it's a path, if it
+        # points to something (exists), otherwise we'll assume that it's the
+        # script's name and try to evaluate its path.
+        _execute = shlex.split(script)
+        p = subprocess.Popen(_execute, stdout=subprocess.PIPE)
+        output, error = p.communicate()
+        output = output.decode('utf-8')
+        try:
+            cb(result=output, *args, **kwargs)
+        except TypeError:  # not callable, maybe?
+            pass
+        return output
 
     def _generate_callback(self, opt):
         """
         Generate a callback function for the app options from parameters
         recovered from the json storing the state.
         """
-        label = 'Processing callback for {}'.format(opt['callback'])
+        label = 'Processing callback for {}'.format(opt.get('script'))
 
         def cb(*args, **kwargs):
             with click.progressbar(range(5), label=label) as bar:
@@ -68,26 +114,50 @@ class AppManager(object):
         return idx
 
     def find_option_idx(self, value, key='text'):
-        """Find an option index. """
-        for i, v in enumerate(self.options):
-            try:
-                value = value.lower()
-            except AttributeError:
-                pass
+        """
+        Return an option index inside the app options using a key-value pair.
+        """
+        return self._find_option_idx(value, key, self.options)
 
-            if v.get(key, '').lower() == value:
+    def _find_json_option_idx(self, value, key='text'):
+        """
+        Find the index of the option inside the stored unmodified data recovered
+        by the state.json file.
+        """
+        return self._find_option_idx(value, key, self._options)
+
+    def _find_option_idx(self, value, key, option_attr):
+        """Find an option index. """
+        for i, v in enumerate(option_attr):
+            if v.get(key, '').lower() == value.lower():
                 return i
         return None
 
-    def update_option(self,  name=None, idx=None, value=None, *args, **kwargs):
+    def update_option(self, name=None, idx=None, *args, **kwargs):
         """
         Update the `installed` attribute of the given option, either by
         `name` ('text' attribute) or by `idx` (index in the option list)
         """
-        if not name and not idx:
-            raise ValueError('Missing option <idx> or <text> values')
         if name and not idx:
-            idx = self.find_option_idx(name)
+            public_idx = self.find_option_idx(name)
+            private_idx = self._find_json_option_idx(name)
+        elif idx and not name:
+            public_idx = idx
+            name = self.options[public_idx]['text']
+            private_idx = self._find_json_option_idx(name)
+        else:
+            raise ValueError('Missing option <idx> or <text> values')
 
-        value = value or datetime.now().strftime('%d/%m/%Y')
-        self.options[idx]['installed'] = value
+        if not public_idx:
+            raise IndexError('Option index out of range')
+
+        install_date = datetime.now().strftime('%d/%m/%Y')
+
+        self.options[public_idx]['installed'] = install_date
+        try:
+            self._options[private_idx]['installed'] = install_date
+        except KeyError:  # optiona added at runtime
+            pass
+
+        with click.open_file(self._app_state_path, 'w', atomic=True) as fo:
+            json.dump(self._options, fo, indent=4)
