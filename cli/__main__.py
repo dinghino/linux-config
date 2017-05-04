31b5c1ac0f800b717cb519558a752d1337e977da
Daniele Ding <dinghino@gmail.com>
2017-05-04 23:21:26 +0200

    add __main__.py with pip installations

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,3 +1,35 @@
-from cli import cli
+# -*- coding: utf-8 -*-
+import importlib
+import os
+import site
 
+import pip
+import six
+
+cwd = os.path.dirname(__file__)
+reqfp = os.path.join(cwd, 'requirements.txt')
+with open(reqfp) as fo:
+    requirements = fo.read().split('\n')[:-1]  # last is an empty string
+
+
+def install_if_not_found(package):
+    try:
+        importlib.import_module(package)
+        six.print_('- %s ok...' % package)
+    except ImportError:
+        six.print_('!! %s not found.  installing...')
+        pip.main(['install', package, '-no-cache-dir'])
+        six.print_('  installed.')
+        # update packages cache
+        reload(site)
+    finally:
+        globals()[package] = importlib.import_module(package)
+
+
+for package in requirements:
+    install_if_not_found(package.split('==')[0])
+
+
+# Execute the program
+from cli import cli
 cli()
